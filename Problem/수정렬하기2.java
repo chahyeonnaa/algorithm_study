4d1bfea3da246e545808650440530fd6ce61586e
chahyeonnaa <gusdk21650@naver.com>
2022-01-25 14:28:17 +0900

    :tulip: 백준/2751:내용 추가(#24)

--- Problem/수정렬하기2.java
+++ Problem/수정렬하기2.java
@@ -1,4 +1,4 @@
-package Problem;
+package algorithm_study.Problem;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,7 +14,31 @@ public class 수정렬하기2 {
         BufferedReader sc=new BufferedReader(new InputStreamReader(System.in));
         StringBuilder st= new StringBuilder();
 
+        // 2. 카운팅 정렬으로 정렬하기
+        // 문제에서 절댓값이 1,000,000보다 작거나 같은 정수라고 명시
+        // index는 0부터 2,000,000까지 -> 이것의 중간인덱스 : 1,000,000
+        boolean arr[]=new boolean[2000001];
+
         int N=Integer.parseInt(sc.readLine());
+
+        for(int i=0;i<N;i++)
+        {
+            // 입력된 정수를 인덱스로 하여 그칸만 true로 바꿔주기 -> 이게 카운팅정렬
+            arr[Integer.parseInt(sc.readLine())+1000000]=true;
+        }
+
+        for(int i=0;i<arr.length;i++)
+        {
+            // 트루인것만 검사_ 배열 인덱스를 앞에서 부터 차례대로 돌기때문에 인덱스를 추가해주면 끝
+            if(arr[i]==true)
+            {
+                st.append(i-1000000).append("\n");
+            }
+        }
+        System.out.println(st);
+
+        // 1. Collections.sort 이용
+/*        int N=Integer.parseInt(sc.readLine());
         ArrayList<Integer> list = new ArrayList<>();
 
         for(int i = 0; i < N; i++) {
@@ -26,7 +50,7 @@ public class 수정렬하기2 {
         for(int value : list) {
             st.append(value).append('\n');
         }
-        System.out.println(st);
+        System.out.println(st);*/
 
 
 

10f0330023df332075cd7162c2dc4b3b23d4bce1
chahyeonnaa <gusdk21650@naver.com>
2022-02-13 18:48:06 +0900

    :tulip: [Lv1]프로그래머스:수정 중(#81)

--- Problem/실패율.java
+++ Problem/실패율.java
@@ -11,23 +11,47 @@ public class 실패율 {
 
         for(int i=0;i<stages.length;i++)
         {
-            if(t_answer[stages[i]]==false)
+            if(t_answer[stages[i]]==true)
                 continue;
-            double count1=0;
-            double count2=0;
+            int count1=0;
+            int count2=0;
+
             for(int j=0;j<stages.length;j++)
             {
-                if(stages[i]>=stages[j])
+                if(stages[i]<=stages[j])
                     count1++;
 
-                if(stages[i]==stages[j])
+                if(stages[i]==N+1)
+                    count2=0;
+                else if(stages[i]==stages[j])
                     count2++;
             }
-            t_answer[stages[i]]=false;
-            result[stages[i]]=count1/count2;
+            t_answer[stages[i]]=true;
+            if(stages[i]==N+1)
+            {
+                result[stages[i]-1]=(double)count2/(double)count1;
+            }
+            else
+                result[stages[i]]=(double)count2/(double)count1;
         }
 
         // 최댓값 찾아서 그때의 인덱스를 answer배열에 넣어주기
+
+        int maxIndex = 0;
+        // 최대값, 최대값의 인덱스 구하기
+        for(int j=0;j<N;j++)
+        {
+            double max = -1;
+            for (int i = 1; i < result.length; i++)
+            {
+                if (result[i] > max) {
+                    max = result[i];
+                    maxIndex = i;
+                }
+            }
+            result[maxIndex]=-3;
+            answer[j]=maxIndex;
+        }
 
         return answer;
     }

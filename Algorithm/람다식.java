2c1a2cb3c6c8f9263bd2000330dad33d0f042a9a
chahyeonnaa <gusdk21650@naver.com>
2022-02-18 15:21:34 +0900

    :four_leaf_clover: 람다식

---
+++ Algorithm/람다식.java
@@ -0,0 +1,23 @@
+package algorithm_study.Algorithm;
+
+public class 람다식 {
+
+/*    // 람다식 X
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    // 람다식 O
+    (int a, int b) -> {return a + b;}
+
+    // -------------------------------------------
+
+    // 람다식 X
+    int c = sum(a, b);
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    // 람다식 O
+    int c = (int a, int b) -> {return a + b;}*/
+}

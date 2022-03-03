c6c2af9cbd376d2622dab7cf1ddb98a83f458734
chahyeonnaa <gusdk21650@naver.com>
2022-03-03 15:05:55 +0900

    :tulip: [Lv1]/프로그래머스

---
+++ Problem/나머지가_1이되는수_찾기.java
@@ -0,0 +1,17 @@
+package algorithm_study.Problem;
+
+public class 나머지가_1이되는수_찾기 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==1)
+            {
+                answer=i;
+                break;
+            }
+
+        }
+        return answer;
+    }
+}
